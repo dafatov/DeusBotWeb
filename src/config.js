7d6272bed12da04c97e72e584baeb1e6c01f555c
dafatov <dafatov@yandex.ru>
2022-05-28 23:01:51 +0300

    Try 7 another App.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
-let configLocal;
-
-try {
-  configLocal = require("./configLocal");
-} catch (e) {
-  console.log(e)
-}
+// import configLocal from "./configLocal";
 
 module.exports = {
-  'client_uri': process.env.CLIENT_URI || configLocal.client_uri,
-  'server_uri': process.env.SERVER_URI || configLocal.server_uri,
-  'discord_api_url': process.env.DISCORD_API_URL || configLocal.discord_api_url,
-  'discord_cdn_url': process.env.DISCORD_CDN_URL || configLocal.discord_cdn_url,
-  'client_id': process.env.CLIENT_ID || configLocal.client_id,
-  'client_secret': process.env.CLIENT_SECRET || configLocal.client_secret,
+  'client_uri': process.env.CLIENT_URI,
+  'server_uri': process.env.SERVER_URI,
+  'discord_api_url': process.env.DISCORD_API_URL,
+  'discord_cdn_url': process.env.DISCORD_CDN_URL,
+  'client_id': process.env.CLIENT_ID,
+  'client_secret': process.env.CLIENT_SECRET,
+
+  // 'client_uri': configLocal.client_uri,
+  // 'server_uri': configLocal.server_uri,
+  // 'discord_api_url': configLocal.discord_api_url,
+  // 'discord_cdn_url': configLocal.discord_cdn_url,
+  // 'client_id': configLocal.client_id,
+  // 'client_secret':configLocal.client_secret,
 }
