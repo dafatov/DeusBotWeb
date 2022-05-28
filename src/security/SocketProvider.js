c7127c421a35fb135e76b4abcb24c46eae42c7b2
dafatov <dafatov@yandex.ru>
2022-05-28 23:08:27 +0300

    Try 7 another App.js

--- src/security/SocketProvider.js
+++ src/security/SocketProvider.js
@@ -1,22 +1,21 @@
-//import {io} from "socket.io-client";
-import {createContext, useContext} from "react";
+import {io} from "socket.io-client";
+import {createContext, useContext, useEffect, useRef} from "react";
 import {server_uri} from "../config";
 
 const SocketContext = createContext({});
 
 export const SocketProvider = ({children}) => {
-  const socket = server_uri/*io(`ws://${server_uri}`);*/
+  const socket = useRef(null);
 
-  //useEffect(() => {socket.current = io(`ws://${server_uri}`)}, [])
+  useEffect(() => {socket.current = io(`ws://${server_uri}`)}, [])
 
   return (
     <SocketContext.Provider
       value={socket}
     >
       {children}
-      {socket}
     </SocketContext.Provider>
   );
 };
 
-export const useSocket = () => useContext(SocketContext)/*.current*/;
+export const useSocket = () => useContext(SocketContext).current;
