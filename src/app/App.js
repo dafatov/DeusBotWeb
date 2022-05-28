34640671ccb6d8c47a0778f6ef6cfb9afb7fd013
dafatov <dafatov@yandex.ru>
2022-05-28 20:16:55 +0300

    Try 5 another App.js

--- src/app/App.js
+++ src/app/App.js
@@ -2,26 +2,29 @@ import {ThemeProvider} from "@mui/material/styles";
 import theme from "../styles/theme";
 import {SnackbarProvider} from "notistack";
 import {SnackBarProviderProps} from "../utils/SnackBar";
+import {SocketProvider} from "../security/SocketProvider";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider {...SnackBarProviderProps}>
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <SocketProvider>
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </SocketProvider>
       </SnackbarProvider>
       </ThemeProvider>
   );
