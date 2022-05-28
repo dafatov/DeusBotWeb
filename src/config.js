7ce94f0fe99e89653d92ba30448c7a0ce0975b1f
dafatov <dafatov@yandex.ru>
2022-05-28 17:00:11 +0300

    Added try/catch block for require in config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
-const {client_id, client_secret, client_uri, discord_api_url, discord_cdn_url, server_uri} = require("./configLocal");
+let configLocal;
+
+try {
+  configLocal = require("./configLocal");
+} catch (e) {
+  console.log(e)
+}
 
 module.exports = {
-  'client_uri': process.env.CLIENT_URI || client_uri,
-  'server_uri': process.env.SERVER_URI || server_uri,
-  'discord_api_url': process.env.DISCORD_API_URL || discord_api_url,
-  'discord_cdn_url': process.env.DISCORD_CDN_URL || discord_cdn_url,
-  'client_id': process.env.CLIENT_ID || client_id,
-  'client_secret': process.env.CLIENT_SECRET || client_secret,
+  'client_uri': process.env.CLIENT_URI || configLocal.client_uri,
+  'server_uri': process.env.SERVER_URI || configLocal.server_uri,
+  'discord_api_url': process.env.DISCORD_API_URL || configLocal.discord_api_url,
+  'discord_cdn_url': process.env.DISCORD_CDN_URL || configLocal.discord_cdn_url,
+  'client_id': process.env.CLIENT_ID || configLocal.client_id,
+  'client_secret': process.env.CLIENT_SECRET || configLocal.client_secret,
 }
